08b82de396abc3cdfde1e09f51f74a70e40b2612
Heesu Kang <hskang135@gmail.com>
2019-11-13 17:10:19 -0500

    done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import Header from '../Header/Header'
-// import PrivateRoute from '../Utils/PrivateRoute'
-// import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
-import ThingListPage from '../../routes/ThingListPage/ThingListPage'
-import ThingPage from '../../routes/ThingPage/ThingPage'
-import LoginPage from '../../routes/LoginPage/LoginPage'
-import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
-import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
-import './App.css'
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Header from '../Header/Header';
+import PrivateRoute from '../Utils/PrivateRoute';
+import PublicOnlyRoute from '../Utils/PublicOnlyRoute';
+import ThingListPage from '../../routes/ThingListPage/ThingListPage';
+import ThingPage from '../../routes/ThingPage/ThingPage';
+import LoginPage from '../../routes/LoginPage/LoginPage';
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage';
+import './App.css';
 
 class App extends Component {
   state = { hasError: false }
@@ -32,15 +32,15 @@ class App extends Component {
               path={'/'}
               component={ThingListPage}
             />
-            <Route
+            <PublicOnlyRoute
               path={'/login'}
               component={LoginPage}
             />
-            <Route
+            <PublicOnlyRoute
               path={'/register'}
               component={RegistrationPage}
             />
-            <Route
+            <PrivateRoute
               path={'/thing/:thingId'}
               component={ThingPage}
             />
